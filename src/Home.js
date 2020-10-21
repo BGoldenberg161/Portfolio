039aa126e063f30df5ae67b7f70f36a54cd4e0e1
Branden Goldenberg <brandengoldenberg@MacBook-Pro.local>
2020-10-21 09:41:46 -0600

    add skeleton for components

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const Home = () => {
+  return (
+    <div className="App">
+      <h1>Image of me</h1>
+      <h1>Cool background effect</h1>
+      <h1>Small blurb about myself</h1>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Projects.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+const Projects = () => {
+  return (
+    <div className="App">
+      <h1>Images of projects</h1>
+      <h1>Cool animation effect</h1>
+      <h1>Small blurb about each project</h1>
+    </div>
+  );
+}
+
+export default Projects;
