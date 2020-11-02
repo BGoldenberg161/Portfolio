f3441daabd9fa58eebcbf8a227055c85c5bf4c6f
Branden Goldenberg <brandengoldenberg@MacBook-Pro.local>
2020-11-02 15:23:17 -0700

    add parallax effect to projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import '../App.css'
+import M from "materialize-css"
 import class1 from '../images/class1.png'
 import class2 from '../images/class2.png'
 import apartmate1 from '../images/apartmate1.png'
@@ -11,15 +12,19 @@ import superskierbro1 from '../images/superskierbro1.png'
 import superskierbro2 from '../images/superskierbro2.png'
 
 const Projects = () => {
+
+  useEffect(() => {
+    const elems = document.querySelectorAll('.parallax')
+    const instances = M.Parallax.init(elems);
+  }, [])
+
   return (
     <div className="projects">
-      <img src={class1} alt="class1" width="50%"></img>
-      <img src={class2} alt="class2" width="50%"></img>
-      <p>
-        technologies used: 
-        Node, Python, Django, Django-rest-framework, jwt-tokens, 
-        Javascript, React, Grommet, Postgresql, express
-      </p>
+      <div className="parallax-container">
+        <div className="parallax">
+          <img src={class1} alt="class1" width="50%"></img>
+        </div>
+      </div>
       <p>
         A full stack application with both teachers and students 
         in mind, a school system platform created to help streamline 
@@ -27,12 +32,16 @@ const Projects = () => {
         with ease. The beginging of a true one-stop shop, 
         welcome to Class.
       </p>
-      <img src={apartmate1} alt="apartmate1" width="50%"></img>
-      <img src={apartmate2} alt="apartmate2" width="50%"></img>
       <p>
-        technologies used:  
-        Node, Javascript, React, MaterialUI, MongoDB, passport 
+        technologies used: 
+        Node, Python, Django, Django-rest-framework, jwt-tokens, 
+        Javascript, React, Grommet, Postgresql, express
       </p>
+      <div className="parallax-container">
+        <div className="parallax">
+          <img src={apartmate1} alt="apartmate1" width="50%"></img>
+        </div>
+      </div>
       <p>
         Apartmate was built to solve the awkward roommate problems we all run into. 
         Nobody wants to be a "Karen" and lecture their roommates about doing basic 
@@ -42,13 +51,15 @@ const Projects = () => {
         member and to top it off, you can easily split Venmo payments by inputting 
         the amount and your roomates will get a text with the exact Venmo amount they need to pay you.
       </p>
-      <img src={emulsio1} alt="emulsio1" width="100%"></img>
-      <img src={emulsio2} alt="emulsio2" width="50%"></img>
-      <img src={emulsio3} alt="emulsio3" width="50%"></img>
       <p>
         technologies used:  
-        Node, Javascript, Materialize, Postgresql, Express, ejs
+        Node, Javascript, React, MaterialUI, MongoDB, passport 
       </p>
+      <div className="parallax-container">
+        <div className="parallax">
+          <img src={emulsio1} alt="emulsio1" width="100%"></img>
+        </div>
+      </div>
       <p>
         Cooking with dietary restrictions is always a challange. Finding a recipe, 
         figuring out what needs to be removed, how to replace it, etc. Emulsio is 
@@ -58,16 +69,23 @@ const Projects = () => {
         has been optimized for mobile to ease the use of the app while actually 
         working in the kitchen!
       </p>
-      <img src={superskierbro1} alt="superskierbro1" width="50%"></img>
-      <img src={superskierbro2} alt="superskierbro2" width="50%"></img>
       <p>
         technologies used:  
-        Javascript, HTML5, CSS3
+        Node, Javascript, Materialize, Postgresql, Express, ejs
       </p>
+      <div className="parallax-container">
+        <div className="parallax">
+          <img src={superskierbro2} alt="superskierbro1" width="50%"></img>
+        </div>
+      </div>
       <p>
         An old school SuperMario style game where the player can jump on "bad guys" 
         to do damage while gaining experience points to level up. All the artwork was 
         also hand drawn. A truly from scratch mini game!
+      </p>
+      <p>
+        technologies used:  
+        Javascript, HTML5, CSS3
       </p>
     </div>
   );
